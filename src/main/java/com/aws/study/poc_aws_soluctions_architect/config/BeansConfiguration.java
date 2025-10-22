460c6e10211bc9494a27e982ee38a70bfa6481a9
Daniel Ramos <dramosg@ucentral.edu.co>
2025-10-21 21:27:41 -0500

    feat:Implement event publishing mechanism with bounded executor and async processing for comments

--- src/main/java/com/aws/study/poc_aws_soluctions_architect/config/BeansConfiguration.java
+++ src/main/java/com/aws/study/poc_aws_soluctions_architect/config/BeansConfiguration.java
@@ -1,5 +1,6 @@
-package com.aws.study.poc_aws_soluctions_architect.config;
+package com.aws.study.poc_aws_soluitions_architect.config;
 
+import com.aws.study.poc_aws_soluctions_architect.config.executor.BoundedExecutor;
 import feign.Client;
 import feign.hc5.ApacheHttp5Client;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
@@ -8,9 +9,18 @@ import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Configuration
+@EnableAsync
 public class BeansConfiguration {
+
+    private final ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
 
     @Bean
     public Client feignClient() {
@@ -24,4 +34,13 @@ public class BeansConfiguration {
                 .build();
         return new ApacheHttp5Client(httpClient);
     }
+
+    @Bean(name = "applicationEventMulticaster")
+    public ApplicationEventMulticaster simpleApplicationEventMulticaster() {
+        SimpleApplicationEventMulticaster eventMulticaster =
+                new SimpleApplicationEventMulticaster();
+        eventMulticaster.setTaskExecutor(new BoundedExecutor(executor, 50));
+        return eventMulticaster;
+    }
+
 }
