568385ed132ba75c5ed497a316a94671c02f7341
Daniel Ramos <dramosg@ucentral.edu.co>
2025-10-21 22:20:26 -0500

    feat:Enable Feign clients and async processing in application configuration

--- src/main/java/com/aws/study/poc_aws_soluctions_architect/config/BeansConfiguration.java
+++ src/main/java/com/aws/study/poc_aws_soluctions_architect/config/BeansConfiguration.java
@@ -11,13 +11,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Configuration
-@EnableAsync
 public class BeansConfiguration {
 
     private final ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
